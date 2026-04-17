c35129d5c79267455125c6ef9e309d05cc5ea89f
Ashik Jishy <ashikjishy001@gmail.com>
2026-04-17 09:56:16 +0530

    Time: 284 ms (48.84%), Space: 42.1 MB (51.16%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.py
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def rev_num(self, num):
         rev = 0
-        while num>0:
-            rev = (rev * 10) + num%10
+        while num > 0:
+            rev = (rev * 10) + num % 10
             num //= 10
         return rev
 
@@ -11,7 +11,9 @@ class Solution:
         dis = float("inf")
         for i in range(len(nums)):
             if nums[i] in hash_map:
-                dis = min(dis , abs(i - hash_map[nums[i]]))
+                dis = min(dis, abs(i - hash_map[nums[i]]))
+                if dis == 1:
+                    return dis
             hash_map[self.rev_num(nums[i])] = i
-        
+
         return dis if dis != float("inf") else -1
